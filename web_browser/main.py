46f35cd0a3179dad719931c2cb89e350b960713a
RedZunder <alt47r@gmail.com>
2025-05-25 16:24:17 +0200

    Added history
    History in side panel view

--- web_browser/main.py
+++ web_browser/main.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QPushButton
+from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtCore import QUrl
 
@@ -18,6 +18,10 @@ class WebBrowser(QWidget):
         self.url_bar.setPlaceholderText("Enter URL...")
         self.url_bar.returnPressed.connect(self.navigate_to_url)
 
+        self.history_btn = QPushButton("History")
+        self.history_btn.setCheckable(True)
+        self.history_btn.clicked.connect(self.show_history)
+
         self.back_btn = QPushButton("<-")
         self.back_btn.clicked.connect(self.go_back)
 
@@ -27,6 +31,7 @@ class WebBrowser(QWidget):
         self.go_btn = QPushButton("Go")
         self.go_btn.clicked.connect(self.navigate_to_url)
 
+        self.nav_bar_layout.addWidget(self.history_btn)
         self.nav_bar_layout.addWidget(self.back_btn)
         self.nav_bar_layout.addWidget(self.forward_btn)
         self.nav_bar_layout.addWidget(self.url_bar)
@@ -36,13 +41,21 @@ class WebBrowser(QWidget):
         self.browser.setUrl(QUrl("http://duckduckgo.com"))
         self.browser.urlChanged.connect(self.update_url_bar)
 
+        self.content_layout = QHBoxLayout()
+        self.history_list = QListWidget()
+        self.history_list.setVisible(False)
+
+        #prepare layout
+        self.content_layout.addWidget(self.history_list)
+        self.content_layout.addWidget(self.browser)
         self.main_layout.addLayout(self.nav_bar_layout)
-        self.main_layout.addWidget(self.browser)
-        self.setLayout(self.main_layout)
+        self.main_layout.addLayout(self.content_layout)
+
+        self.setLayout(self.main_layout)            #apply layout
 
     def navigate_to_url(self):
         url = self.url_bar.text().strip()
-        if not url.startswith(("http://")):
+        if not url.startswith(("http")):
             url = "http://" + url
         self.browser.setUrl(QUrl(url))
 
@@ -54,6 +67,16 @@ class WebBrowser(QWidget):
 
     def go_forward(self):
         self.browser.forward()
+
+    def show_history(self, checked):
+        # toggle visibility
+        if checked:
+            self.history_list.clear()
+            for pages in self.browser.history().items():
+                self.history_list.addItem(f"{pages.title()} — {pages.url().toString()}")
+            self.history_list.setVisible(True)
+        else:
+            self.history_list.setVisible(False)
 
 
 if __name__ == "__main__":
